c45db75fce7d1da272b7c7cd353a9da86101f033
gabiramire <45242292+gabiramire@users.noreply.github.com>
2019-03-20 21:27:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,31 +6,24 @@ public class Main {
 		double res;
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Insira o primeiro número inteiro");
+		System.out.println("Insira o primeiro nÃºmero inteiro");
 		n1 = input.nextInt();
-		System.out.println("Insira o segundo número inteiro");
+		System.out.println("Insira o segundo nÃºmero inteiro");
 		n2 = input.nextInt();
 		
 		Operacao operacao = new Operacao(n1, n2);
 
 		operacao.soma();
-		System.out.println("O resultado da soma é: "+ operacao.getResultado());
+		System.out.println("O resultado da soma Ã©: "+ operacao.getResultado());
 		
 		operacao.calcularMedia(n1, n2);
-		System.out.println("A média dos números é: "+ operacao.getResultado());
+		System.out.println("A mÃ©dia dos nÃºmeros Ã©: "+ operacao.getResultado());
 		
 		res = operacao.multiplicar();
-		System.out.println("O resultado da multiplicação dos números é: "+ res);
+		System.out.println("O resultado da multiplicaÃ§Ã£o dos nÃºmeros Ã©: "+ res);
 
 		res = operacao.dividir(n1, n2);
-		System.out.println("O resultado da divisão dos números é: "+ res);	
-		
-		operacao.subtrair();
-		System.out.println("O resultado da subtração é: "+ operacao.getResultado());
-		
-		res= operacao.calcularPotencia();
-		System.out.println("O resultado da potência dos números é: "+ res);
-		
+		System.out.println("O resultado da divisÃ£o dos nÃºmeros Ã©: "+ res);	
 		
 	}
 }
